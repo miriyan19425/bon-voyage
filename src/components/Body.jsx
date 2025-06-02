b0dddd78b2bafeed61a3e89be9c63b5563988665
miriyan19425 <miriyan19425@gmail.com>
2025-06-02 22:28:12 +0300

    Create Body.jsx

---
+++ src/components/Body.jsx
@@ -0,0 +1,15 @@
+import Welcome  from  './Welcome';
+import Wrapper  from  './Wrapper';
+import CompleteVacation  from  './CompleteVacation';
+
+function Body(){
+    return (
+        <main className='body'>
+            <Welcome />
+            <Wrapper />
+            <CompleteVacation />
+        </main>
+    );
+}
+
+export default Body;
