06e39905f22b974558495901730641848c4901ff
miriyan19425 <miriyan19425@gmail.com>
2025-06-03 21:53:00 +0300

    Create InfoVacations.jsx

---
+++ src/components/InfoVacations.jsx
@@ -0,0 +1,14 @@
+function InfoVacations(){
+    return (
+        <section id="info-vacations">
+        <div class="vacations-info-container">
+          <div class="wrapper">
+            <h1>Vacation info</h1>
+            <ul class="info-list"></ul>
+          </div>
+        </div>
+      </section>
+    );
+}
+
+export default InfoVacations;
