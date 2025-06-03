61d6631997bed121136165a18527e9fbb162cd81
miriyan19425 <miriyan19425@gmail.com>
2025-06-03 21:50:30 +0300

    Create ConfirmVacations.jsx

---
+++ src/components/ConfirmVacations.jsx
@@ -0,0 +1,14 @@
+function ConfirmVacations(){
+    return (
+        <section id="confirm-vacations">
+        <div class="confirm-container">
+          <div class="wrapper">
+            <h1>Confirm Vacation</h1>
+            <ul class="confirm-list"></ul>
+          </div>
+        </div>
+      </section>
+    );
+}
+
+export default ConfirmVacations;
