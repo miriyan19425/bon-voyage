cdf5716f9e5fff230402808f8712b84f081d5918
miriyan19425 <miriyan19425@gmail.com>
2025-06-03 21:49:27 +0300

    Create CompleteVacation.jsx

---
+++ src/components/CompleteVacation.jsx
@@ -0,0 +1,13 @@
+function CompleteVacation(){
+    return (
+        <section id="complete-vacation">
+            <div>
+                <a id="ok">
+                    <h1 id="status"></h1>
+                </a>
+            </div>
+        </section>
+    );
+}
+
+export default CompleteVacation;
