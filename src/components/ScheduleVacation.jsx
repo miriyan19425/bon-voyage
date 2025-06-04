512d4335f7e9cb31526cc5cbe0c44cd4b8e15921
miriyan19425 <miriyan19425@gmail.com>
2025-06-04 21:52:54 +0300

    Create ScheduleVacation.jsx

---
+++ src/components/ScheduleVacation.jsx
@@ -0,0 +1,29 @@
+function ScheduleVacation(){
+    return (
+      <section id="schedule-vacation">
+        <div class="wrapper">
+          <h1>Vacation Schedule</h1>
+
+          <div class="container-text">
+            <form action="">
+              <label for="first-name">First name:</label>
+              <input type="text" id="first-name" name="first-name" />
+
+              <label for="last-name">Last name:</label>
+              <input type="text" id="last-name" name="last-name" />
+
+              <label for="from-date">From Date:</label>
+              <input type="date" id="from-date" name="from-date" />
+
+              <label for="to-date">To Date:</label>
+              <input type="date" id="to-date" name="to-date" />
+
+              <button id="next-btn" type="submit">Next</button>
+            </form>
+          </div>
+        </div>
+      </section>
+    );
+}
+
+export default ScheduleVacation;
