abe68deb28cb12655ca47d03c1280ac7d6aab954
miriyan19425 <miriyan19425@gmail.com>
2025-06-04 21:53:41 +0300

    Create Welcome.jsx

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,14 @@
+function Welcome(){
+    return (
+        <section id="welcome">
+            <h1>Bon Voyage!</h1>
+            <div class="home-container">
+                <div class="info">
+                    <h1>Vacation Schedule</h1>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Welcome;
