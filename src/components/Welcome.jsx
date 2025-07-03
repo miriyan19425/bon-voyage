4692a2563a6c0b65848aa067b3a75090379d97de
miriyan19425 <miriyan19425@gmail.com>
2025-07-04 00:35:53 +0300

    Update Welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,9 +1,11 @@
+import styles from './Welcome.module.css';
+
 function Welcome(){
     return (
         <section id="welcome">
             <h1>Bon Voyage!</h1>
-            <div class="home-container">
-                <div class="info">
+            <div class={styles.homeContainer}>
+                <div class={styles.info}>
                     <h1>Vacation Schedule</h1>
                 </div>
             </div>
