ac9b35b467d132a9d5f646321f1d515e1f9fd1ab
miriyan19425 <miriyan19425@gmail.com>
2025-08-08 22:58:36 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,26 @@
 import VacationForm from "./components/VacationForm";
+import VacationInfo from "./components/VacationInfo";
+import ConfirmedVacation from "./components/ConfirmedVacation";
+import CompletedVacation from "./components/CompletedVacation";
 
 function App() {
    return (
-      <div className="body">
-         <section id="welcome">
-            <h1>Bon Voyage!</h1>
-            <div class="home-container">
-               <div class="info">
-                  <h1>Vacation Schedule</h1>
-               </div>
-            </div>
-         </section>
-         <div id="wrapper">
-            <VacationForm />
-            <section id="info-vacations">
-               <div class="vacations-info-container">
-                  <div class="wrapper">
-                     <h1>Vacation info</h1>
-                     <ul class="info-list"></ul>
-                  </div>
-               </div>
-            </section>
-            <section id="confirm-vacations">
-               <div class="confirm-container">
-                  <div class="wrapper">
-                     <h1>Confirm Vacation</h1>
-                     <ul class="confirm-list"></ul>
-                  </div>
-               </div>
-            </section>
-         </div>
-         <section id="complete-vacation">
-      <div>
-        <a id="ok">
-          <h1 id="status"></h1>
-        </a>
+        <div className="body">
+    <section id="welcome">
+      <h1>Bon Voyage!</h1>
+      <div class="home-container">
+        <div class="info">
+          <h1>Vacation Schedule</h1>
+        </div>
       </div>
     </section>
 
-    <script src="app.js"></script>
+    <div id="wrapper">
+      <VacationForm />
+      <VacationInfo />
+      <ConfirmedVacation />
+    </div>
+    <CompletedVacation />
   </div>
    );
 }
