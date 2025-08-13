b3567d5fc5f1c40ccc93b710a824a496fa0392cf
miriyan19425 <miriyan19425@gmail.com>
2025-08-14 02:15:29 +0300

    Update ConfirmedVacation.jsx

--- src/components/ConfirmedVacation.jsx
+++ src/components/ConfirmedVacation.jsx
@@ -1,13 +1,13 @@
 function ConfirmedVacation(){
     return (
         <section id="confirm-vacations">
-        <div class="confirm-container">
-          <div class="wrapper">
-            <h1>Confirm Vacation</h1>
-            <ul class="confirm-list"></ul>
-          </div>
-        </div>
-      </section>
+            <div class="confirm-container">
+                <div class="wrapper">
+                    <h1>Confirm Vacation</h1>
+                    <ul class="confirm-list"></ul>
+                </div>
+            </div>
+        </section>
     );
 }
 
