516b4582f4d6417c1a5d32325f8f18183b707db2
miriyan19425 <miriyan19425@gmail.com>
2025-08-26 23:17:18 +0300

    Update VacationForm.jsx

--- src/components/VacationForm.jsx
+++ src/components/VacationForm.jsx
@@ -2,6 +2,7 @@ import {useState} from 'react';
 
 function VacationForm({prepareVacation}){
     const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
     
     return (
         <section id="schedule-vacation">
