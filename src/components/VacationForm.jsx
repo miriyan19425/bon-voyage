b1755e5db18d9a1e704d352201be67a020456877
miriyan19425 <miriyan19425@gmail.com>
2025-09-03 00:24:27 +0300

    Update VacationForm.jsx

--- src/components/VacationForm.jsx
+++ src/components/VacationForm.jsx
@@ -14,6 +14,7 @@ function VacationForm({prepareVacation}){
         const data = {firstName, lastName, from, to};
 
         prepareVacation(data);
+        setFirstName("");
     }
     
     return (
